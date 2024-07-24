1af4cc681269d135361d42a0c1f81ffa07630359
young219257 <young219257@gmail.com>
2024-07-24 20:09:43 +0900

    [Refactor] category Type

--- src/main/java/com/sparta/bochodrive/domain/community/service/CommunityService.java
+++ src/main/java/com/sparta/bochodrive/domain/community/service/CommunityService.java
@@ -3,6 +3,7 @@ package com.sparta.bochodrive.domain.community.service;
 import com.sparta.bochodrive.domain.community.dto.CommunityListResponseDto;
 import com.sparta.bochodrive.domain.community.dto.CommunityRequestDto;
 import com.sparta.bochodrive.domain.community.dto.CommunityResponseDto;
+import com.sparta.bochodrive.domain.community.entity.CategoryEnum;
 import com.sparta.bochodrive.domain.user.entity.User;
 import com.sparta.bochodrive.global.UserDetailsImpl;
 import org.checkerframework.checker.units.qual.C;
@@ -12,8 +13,8 @@ import java.util.List;
 
 public interface CommunityService {
     CommunityResponseDto addPost(CommunityRequestDto communityRequestDto, User user);
-    List<CommunityListResponseDto> getAllPosts(String category);
+    List<CommunityListResponseDto> getAllPosts(CategoryEnum category);
     CommunityResponseDto getPost(Long id);
-    CommunityResponseDto updatePost(Long id, CommunityRequestDto communityRequestDto, UserDetailsImpl userDetails) throws AccessDeniedException;
+    void updatePost(Long id, CommunityRequestDto communityRequestDto);
     void deletePost(Long id, UserDetailsImpl userDetails) throws AccessDeniedException;
 }
--- src/main/java/com/sparta/bochodrive/domain/community/service/CommunityServiceImpl.java
+++ src/main/java/com/sparta/bochodrive/domain/community/service/CommunityServiceImpl.java
@@ -3,6 +3,7 @@ package com.sparta.bochodrive.domain.community.service;
 import com.sparta.bochodrive.domain.community.dto.CommunityListResponseDto;
 import com.sparta.bochodrive.domain.community.dto.CommunityRequestDto;
 import com.sparta.bochodrive.domain.community.dto.CommunityResponseDto;
+import com.sparta.bochodrive.domain.community.entity.CategoryEnum;
 import com.sparta.bochodrive.domain.community.entity.Community;
 import com.sparta.bochodrive.domain.community.repository.CommunityRepository;
 import com.sparta.bochodrive.domain.user.entity.User;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.nio.file.AccessDeniedException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -25,6 +27,7 @@ public class CommunityServiceImpl implements CommunityService {
 
 
     //게시글 작성
+    @Override
     public CommunityResponseDto addPost(CommunityRequestDto communityRequestDto, User user) {
         Community community = new Community(communityRequestDto, user);
         Community savedCommunity = communityRepository.save(community);
@@ -33,10 +36,10 @@ public class CommunityServiceImpl implements CommunityService {
     }
 
     //게시글 목록 조회
-    public List<CommunityListResponseDto> getAllPosts(String category) {
+    public List<CommunityListResponseDto> getAllPosts(CategoryEnum category) {
         //카테고리 별로 list찾기
         List<Community> communities;
-        if(category == null || category.isEmpty()) {
+        if(category == null) {
             communities = communityRepository.findAll(); //카테고리 설정 안했을 때 전체글 목록
         }
         else {
@@ -56,19 +59,17 @@ public class CommunityServiceImpl implements CommunityService {
     }
     //게시글 수정
     @Transactional
-    public CommunityResponseDto updatePost(Long id,
-                                           CommunityRequestDto communityRequestDto,
-                                           UserDetailsImpl userDetails) throws AccessDeniedException {
+    public void updatePost(Long id, CommunityRequestDto communityRequestDto) {
 
-        Community community=findCommunityById(id);
-        // 현재 사용자가 게시글 작성자인지 확인
-        if (!community.getUser().equals(userDetails.getUsername())) {
-            throw new AccessDeniedException("게시글 작성자가 아닙니다.");
+        try{
+            Community community=findCommunityById(id);
+            community.update(communityRequestDto);//update
+
+        }catch (IllegalArgumentException e){
+            throw e;
+        }catch (Exception e){
+            throw e;
         }
-        community.update(communityRequestDto); //update
-        communityRepository.save(community); //저장
-
-        return new CommunityResponseDto(community);
     }
 
     //게시글 삭제
@@ -85,7 +86,11 @@ public class CommunityServiceImpl implements CommunityService {
 
     //게시글 id 찾는 메소드
     public Community findCommunityById(Long id) {
-        return communityRepository.findById(id).orElse(null);
+        Optional<Community> community=communityRepository.findById(id);
+        if(community.isEmpty()) {
+            throw new IllegalArgumentException("존재하지 않는 게시글입니다.");
+        }
+        return community.get();
     }
 
 
