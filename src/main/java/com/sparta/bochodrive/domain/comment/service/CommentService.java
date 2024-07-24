3f35118ba656326953c990606246c340115bf05b
young219257 <young219257@gmail.com>
2024-07-24 20:13:02 +0900

    [Refactor] return type 수정

--- src/main/java/com/sparta/bochodrive/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/bochodrive/domain/comment/service/CommentService.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface CommentService {
     CommentReponseDto addComments(CommentRequestDto commentRequestDto, User user);
     List<CommentReponseDto> getComments(Long communitiesId);
-    CommentReponseDto updateComment(Long commentId, CommentRequestDto commentRequestDto, UserDetailsImpl userDetails);
-    void deleteComment(Long commentId, UserDetailsImpl userDetails);
+    void updateComment(Long commentId, CommentRequestDto commentRequestDto);
+    void deleteComment(Long commentId);
 }
