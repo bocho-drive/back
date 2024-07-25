e66669af0d18cc66586273e1a4e6c7cce0c6a331
young219257 <young219257@gmail.com>
2024-07-25 17:35:31 +0900

    [Fix] 예외처리로 인한 버그 수정

--- src/main/java/com/sparta/bochodrive/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/bochodrive/domain/comment/controller/CommentController.java
@@ -24,60 +24,42 @@ public class CommentController {
     //댓글 작성
     @PostMapping
     public ResponseEntity<?> addComment(@RequestBody @Valid CommentRequestDto commentRequestDto,
-                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        try{
-            commentService.addComments(commentRequestDto,userDetails.getUser());
-            Message message = new Message(HttpStatus.CREATED, "댓글 작성에 성공하였습니다.", null);
-            return new ResponseEntity<>(message, HttpStatus.CREATED);
-        }catch (Exception e) {
-            Message message = new Message(HttpStatus.UNAUTHORIZED, "작성에 실패하였습니다.", null);
-            return new ResponseEntity<>(message, HttpStatus.UNAUTHORIZED);
-        }
+                                          @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception{
+
+        commentService.addComments(commentRequestDto,userDetails.getUser());
+        Message message = new Message(HttpStatus.CREATED.value(), "댓글 작성에 성공하였습니다.");
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
+
 
     }
 
     //댓글 전체 조회
     @GetMapping
-    public ResponseEntity<?> getComments(@RequestParam(required = false) Long communitiesId) {
-        try {
-            List<CommentReponseDto> comment = commentService.getComments(communitiesId);
-            return ResponseEntity.ok().body(comment);
-        }catch (Exception e) {
-            Message message = new Message(HttpStatus.UNAUTHORIZED, "조회에 실패하였습니다.", null);
-            return new ResponseEntity<>(message, HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<?> getComments(@RequestParam(required = false) Long communitiesId) throws Exception{
+
+        List<CommentReponseDto> comment = commentService.getComments(communitiesId);
+        return ResponseEntity.ok().body(comment);
+
     }
 
     //댓글 수정
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateComment(@PathVariable Long commentId, @RequestBody @Valid CommentRequestDto commentRequestDto) {
-        try{
-            commentService.updateComment(commentId,commentRequestDto);
-            Message message=new Message(HttpStatus.OK,"수정에 성공하였습니다",null);
-            return new ResponseEntity<>(message,HttpStatus.OK);
-        }catch (IllegalArgumentException e) {
-            Message message=new Message(HttpStatus.NOT_FOUND,"존재하지 않는 댓글입니다.",null);
-            return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
-        }catch (Exception e) {
-            Message message=new Message(HttpStatus.UNAUTHORIZED,"수정에 실패하였습니다.",null);
-            return new ResponseEntity<>(message,HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<?> updateComment(@PathVariable Long commentId, @RequestBody @Valid CommentRequestDto commentRequestDto) throws Exception{
+
+        commentService.updateComment(commentId,commentRequestDto);
+        Message message=new Message(HttpStatus.OK.value(),"수정에 성공하였습니다");
+        return new ResponseEntity<>(message,HttpStatus.OK);
+
     }
 
     //댓글 삭제
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        try{
-            commentService.deleteComment(commentId);
-            Message message=new Message(HttpStatus.OK,"수정에 성공하였습니다",null);
-            return new ResponseEntity<>(message,HttpStatus.OK);
-        }catch (IllegalArgumentException e) {
-            Message message=new Message(HttpStatus.NOT_FOUND,"존재하지 않는 댓글입니다.",null);
-            return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
-        }catch (Exception e) {
-            Message message=new Message(HttpStatus.UNAUTHORIZED,"수정에 실패하였습니다.",null);
-            return new ResponseEntity<>(message,HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<?> deleteComment(@PathVariable Long commentId) throws Exception {
+
+        commentService.deleteComment(commentId);
+        Message message=new Message(HttpStatus.OK.value(),"수정에 성공하였습니다");
+        return new ResponseEntity<>(message,HttpStatus.OK);
+
     }
 
 
