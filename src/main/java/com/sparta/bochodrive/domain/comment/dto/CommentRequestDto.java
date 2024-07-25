635379fac3e38db807d61d2fd4016c4d07695192
young219257 <young219257@gmail.com>
2024-07-25 17:32:15 +0900

    [Add] @Setter 추가

--- src/main/java/com/sparta/bochodrive/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/bochodrive/domain/comment/dto/CommentRequestDto.java
@@ -3,10 +3,12 @@ package com.sparta.bochodrive.domain.comment.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
 public class CommentRequestDto {
 
 
