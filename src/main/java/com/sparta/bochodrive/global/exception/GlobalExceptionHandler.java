c1901d3a96f774716c5aadbb6fd30c080fb824e2
young219257 <young219257@gmail.com>
2024-07-26 01:25:58 +0900

    [Feat] UnauthorizedException 예외 추가

--- src/main/java/com/sparta/bochodrive/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/bochodrive/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sparta.bochodrive.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
 
-    //커스텀하지 않은 모든 예외
+    //커스텀하지 않은 모든 예외 -> 이렇게 지정해주면 알아서 던져준다.
     @ExceptionHandler(Exception.class)
     public ResponseEntity exception(Exception ex) {
         RestApiException restApiException=new RestApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
@@ -37,6 +38,16 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(
                 restApiException,
                 HttpStatus.NOT_FOUND
+        );
+    }
+
+    //403
+    @ExceptionHandler({UnauthorizedException.class})
+    public ResponseEntity<RestApiException> UnauthorizedExceptionHandler(UnauthorizedException ex) {
+        RestApiException restApiException = new RestApiException(HttpStatus.UNAUTHORIZED.value(), ex.getMessage());
+        return new ResponseEntity<>(
+                restApiException,
+                HttpStatus.FORBIDDEN
         );
     }
 }
