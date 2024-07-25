e4b2fe8b7e0079daec7a5f0e39221f5e4b85cfb1
young219257 <young219257@gmail.com>
2024-07-26 00:36:43 +0900

    [Rename] Message -> ApiResponse로 class 이름 수정

---
+++ src/main/java/com/sparta/bochodrive/global/entity/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.sparta.bochodrive.global.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ApiResponse<T> {
+    int statusCode;
+    String message;
+    T data;
+
+
+
+    public ApiResponse(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public static <T> ApiResponse<T> ok(int statusCode, String message, T data) {
+        return new ApiResponse<>(statusCode, message, data);
+    }
+    public static <T> ApiResponse<T> ok(int statusCode, String message) {
+        return new ApiResponse<>(statusCode, message);
+    }
+}
