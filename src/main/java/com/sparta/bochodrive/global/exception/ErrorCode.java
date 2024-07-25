600e6cea0463110f5ff0569072375342a14c2cd6
young219257 <young219257@gmail.com>
2024-07-25 17:47:12 +0900

    [Refactor] @Setter 추가

--- src/main/java/com/sparta/bochodrive/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/bochodrive/global/exception/ErrorCode.java
@@ -1,11 +1,15 @@
 package com.sparta.bochodrive.global.exception;
 
+import lombok.Getter;
+
+
+@Getter
 public enum ErrorCode {
-    BAD_REQUEST(400, "Bad Request"),
+
     POST_NOT_FOUND(404,"존재하지 않는 게시글입니다."),
     COMMENT_NOT_FOUND(404,"존재하지 않는 댓글입니다."),
     ADD_FAILED(401,"등록 실패하였습니다."),
-    GET_FAILED(401, "조회 실패하였습니다."),
+    GETLIST_FAILED(401, "조회 실패하였습니다."),
     UPDATE_FAILED(401,"수정에 실패하였습니다."),
     DELETE_FAILED(401,"삭제에 실패하였습니다."),
     VOTE_FAILED(401,"투표 실패");
