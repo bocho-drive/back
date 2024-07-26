3494a82d245c2385e978e978c09cf86fca75ec14
young219257 <young219257@gmail.com>
2024-07-26 16:27:13 +0900

    [Refactor] UnauthorizedException 삭제

--- src/main/java/com/sparta/bochodrive/domain/admin/entity/Admin.java
+++ src/main/java/com/sparta/bochodrive/domain/admin/entity/Admin.java
@@ -1,15 +1,24 @@
 package com.sparta.bochodrive.domain.admin.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.sparta.bochodrive.domain.user.entity.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Admin {
-//
-//}
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+}
