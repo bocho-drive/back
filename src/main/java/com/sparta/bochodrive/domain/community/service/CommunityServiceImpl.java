62b1dcac449c7b8f1f1b9b60e4f82054f1751dc1
young219257 <young219257@gmail.com>
2024-07-27 01:50:45 +0900

    [Refactor] 예외처리 관련 로직 수정

--- src/main/java/com/sparta/bochodrive/domain/community/service/CommunityServiceImpl.java
+++ src/main/java/com/sparta/bochodrive/domain/community/service/CommunityServiceImpl.java
@@ -10,6 +10,8 @@ import com.sparta.bochodrive.domain.user.entity.User;
 import com.sparta.bochodrive.global.exception.ErrorCode;
 
 import com.sparta.bochodrive.global.exception.NotFoundException;
+import com.sparta.bochodrive.global.exception.UnauthorizedException;
+import com.sparta.bochodrive.global.function.CommonFuntion;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,22 +24,22 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CommunityServiceImpl implements CommunityService {
 
 
     private final CommunityRepository communityRepository;
-
+    private final CommonFuntion commonFuntion;
 
 
     // 게시글 작성
     @Override
+    @Transactional
     public CommunityResponseDto addPost(CommunityRequestDto communityRequestDto, User user)  {
-        Community community = new Community(communityRequestDto, user);
-//        //오늘의 질문!!! ->equals인지 signup뭐시기 인지
-//        if(!community.getUser().getId().equals(user.getId())) {
-//            throw new UnauthorizedException(ErrorCode.DELETE_FAILED);
-//        }
+
+        commonFuntion.existsById(user.getId());
+        Community community = new Community(communityRequestDto,user);
         Community savedCommunity = communityRepository.save(community);
         return new CommunityResponseDto(savedCommunity);
     }
@@ -50,7 +52,7 @@ public class CommunityServiceImpl implements CommunityService {
         List<Community> communities;
         //카테고리별 목록 조회
         if(category == null) {
-            communities = communityRepository.findAll(); //카테고리 설정 안했을 때 전체글 목록
+            communities = communityRepository.findAllByOrderByCreatedAtDesc(); //카테고리 설정 안했을 때 전체글 목록
         }
         else {
             communities=communityRepository.findAllByCategory(category); //카테고리별 글목록
@@ -73,11 +75,14 @@ public class CommunityServiceImpl implements CommunityService {
     @Transactional
     public void updatePost(Long id, CommunityRequestDto communityRequestDto,User user) {
 
+        commonFuntion.existsById(user.getId()); //userId가 userRepository에 존재하는지에 관한 예외처리
         Community community=findCommunityById(id);
-//        //오늘의 질문!!! ->equals인지 signup뭐시기 인지
-//        if(!community.getUser().getId().equals(user.getId())) {
-//            throw new UnauthorizedException(ErrorCode.DELETE_FAILED);
-//        }
+
+        //게시글 community의 userId와 수정하려는 사람의 userId가 같은지에 관한 에외처리
+
+        if(!community.getUser().getId().equals(user.getId())) {
+            throw new UnauthorizedException(ErrorCode.DELETE_FAILED);
+        }
         community.update(communityRequestDto);//update
         communityRepository.save(community);
 
@@ -85,14 +90,13 @@ public class CommunityServiceImpl implements CommunityService {
 
     //게시글 삭제
     @Override
-    @Transactional
     public void deletePost(Long id, User user)  {
 
+        commonFuntion.existsById(user.getId());
         Community community=findCommunityById(id);
-        //
-//        if(!community.getUser().getId().equals(user.getId())) {
-//            throw new UnauthorizedException(ErrorCode.DELETE_FAILED);
-//        }
+        if(!community.getUser().getId().equals(user.getId())) {
+            throw new UnauthorizedException(ErrorCode.DELETE_FAILED);
+        }
         community.setDeleteYn(true);
         communityRepository.save(community);
 
