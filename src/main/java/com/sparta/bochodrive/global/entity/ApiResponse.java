eb1cc81918a404bf0456d0dd25b46ba59fb5a50e
young219257 <young219257@gmail.com>
2024-07-29 21:52:06 +0900

    [Refactor] @Getter 추가

--- src/main/java/com/sparta/bochodrive/global/entity/ApiResponse.java
+++ src/main/java/com/sparta/bochodrive/global/entity/ApiResponse.java
@@ -3,8 +3,10 @@ package com.sparta.bochodrive.global.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class ApiResponse<T> {
     int statusCode;
     String message;
@@ -21,6 +23,10 @@ public class ApiResponse<T> {
         return new ApiResponse<>(statusCode, message, data);
     }
     public static <T> ApiResponse<T> ok(int statusCode, String message) {
+        return new ApiResponse<>(statusCode, message);
+    }
+
+    public static <T> ApiResponse<T> error(int statusCode, String message) {
         return new ApiResponse<>(statusCode, message);
     }
 }
