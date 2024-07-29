48d3ce6083b9413f2b7c89773f913cd38c6d3993
SUIN <65757661+whtndls@users.noreply.github.com>
2024-07-29 16:23:53 +0900

    [Refactor] 로그인 성공 메소드 수정

--- src/main/java/com/sparta/bochodrive/domain/security/filter/LoginFilter.java
+++ src/main/java/com/sparta/bochodrive/domain/security/filter/LoginFilter.java
@@ -1,25 +1,33 @@
 package com.sparta.bochodrive.domain.security.filter;
 
-import com.sparta.bochodrive.domain.security.event.AuthenticationSuccessEvent;
 import com.sparta.bochodrive.domain.security.model.CustomUserDetails;
 import com.sparta.bochodrive.domain.security.utils.JwtUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+@Slf4j
 @RequiredArgsConstructor
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final JwtUtils jwtUtils;
     private final AuthenticationManager authenticationManager;
-    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public LoginFilter(AuthenticationManager authenticationManager, JwtUtils jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtil;
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -35,21 +43,29 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult){
-        CustomUserDetails principal = (CustomUserDetails) authResult.getPrincipal();
-        long userId = principal.getUserId();
-        String username = principal.getUsername();
-        String role = principal.getAuthorities().stream().toList().get(0).getAuthority();
 
-        String accessToken = jwtUtils.createAccessToken(userId, username, role);
-        String refreshToken = jwtUtils.createRefreshToken();
 
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.setHeader("authorization", accessToken);
-        response.setHeader("refresh-token", refreshToken);
+        String username = ((CustomUserDetails) authResult.getPrincipal()).getUsername();
 
-        applicationEventPublisher.publishEvent(new AuthenticationSuccessEvent(this, refreshToken, authResult));
+        /**role 값 가져오기**/
+        Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+
+        log.info("username: {}, role: {}", username, role);
+
+
+        String accessToken = jwtUtils.createAccessToken(username, role);
+
+        //refresh token 안쓸거기때문에 주석처리
+        //String refreshToken = jwtUtils.createRefreshToken();
+
+        response.addHeader("Authorization", "Bearer " + accessToken);
+
     }
+
 
     // 로그인 실패시 호출
     @Override
