0b3cfc62b757561ab7be78640461af2c7fed3d38
SUIN <65757661+whtndls@users.noreply.github.com>
2024-07-29 16:25:30 +0900

    [Refactor] UserController 일반 사용자 로그인 추가

--- src/main/java/com/sparta/bochodrive/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/bochodrive/domain/user/controller/UserController.java
@@ -1,30 +1,54 @@
 package com.sparta.bochodrive.domain.user.controller;
 
+import com.sparta.bochodrive.domain.security.utils.JwtUtils;
+import com.sparta.bochodrive.domain.user.entity.User;
 import com.sparta.bochodrive.domain.user.model.UserModel;
 import com.sparta.bochodrive.domain.user.model.UserModel.UserLoginDto;
 import com.sparta.bochodrive.domain.user.model.UserModel.UserRegistDto;
+import com.sparta.bochodrive.domain.user.repository.UserRepository;
 import com.sparta.bochodrive.domain.user.service.UserService;
 import com.sparta.bochodrive.global.entity.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
+@RequestMapping()
 @RestController
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
+    private final JwtUtils jwtUtils;
+    private final PasswordEncoder passwordEncoder;
 
-    @PostMapping("/signup")
+    @PostMapping("/api/v1/user/signup")
     public UserModel.UserResponseDto postSignUp(@RequestBody UserRegistDto userRegistDto) {
         return userService.registUser(userRegistDto);
     }
 
-    @PostMapping("/signin")
-    public ApiResponse<?> postSignIn(@RequestBody UserLoginDto userLoginDto) {
+    @PostMapping("/api/v1/user/signin")
+    public String postUserSignIn(@RequestBody UserLoginDto userLoginDto) {
+        User user = userRepository.findByEmail(userLoginDto.getEmail()).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 회원입니다.")
+        );
+
+        //비번 맞는지 확인.
+        if(!passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+
+        String nickname = user.getNickname();
+        return jwtUtils.createAccessToken(nickname, "USER");
+    }
+
+    @PostMapping("/api/auth/signin")
+    public ApiResponse<?> postAuthSignIn(@RequestBody UserLoginDto userLoginDto) {
         return null;
     }
 
