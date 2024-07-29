481d1d7d7d0bd4294de711c24c26daee5401d8a8
young219257 <young219257@gmail.com>
2024-07-29 21:51:49 +0900

    [Refactor] get userRole 수정

--- src/main/java/com/sparta/bochodrive/domain/security/model/CustomUserDetails.java
+++ src/main/java/com/sparta/bochodrive/domain/security/model/CustomUserDetails.java
@@ -1,22 +1,40 @@
 package com.sparta.bochodrive.domain.security.model;
 
+import com.sparta.bochodrive.domain.security.enums.UserRole;
 import com.sparta.bochodrive.domain.user.entity.User;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Getter
+@Slf4j
 public class CustomUserDetails implements UserDetails {
 
     private final User user;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(user.getUserRole().getRole()));
+        UserRole userRole = user.getUserRole();
+        if (userRole == null) {
+            log.error("User role is not set for user: {}", user.getEmail());
+            throw new IllegalStateException("User role is not set.");
+        }
+        return Collections.singletonList(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+
+
+
+    public User getUser() {
+        return user;
     }
 
     public long getUserId() { return user.getId(); }
