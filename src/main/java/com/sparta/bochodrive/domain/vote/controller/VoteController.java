681304ad1863170159ff145334999c1d2b6cda30
young219257 <young219257@gmail.com>
2024-07-30 15:00:16 +0900

    [Feat] 중복 투표 예외 로직 추가

--- src/main/java/com/sparta/bochodrive/domain/vote/controller/VoteController.java
+++ src/main/java/com/sparta/bochodrive/domain/vote/controller/VoteController.java
@@ -3,7 +3,7 @@ package com.sparta.bochodrive.domain.vote.controller;
 import com.sparta.bochodrive.domain.security.model.CustomUserDetails;
 import com.sparta.bochodrive.domain.vote.dto.VoteRequestDto;
 
-import com.sparta.bochodrive.domain.vote.service.VoteServiceImpl;
+import com.sparta.bochodrive.domain.vote.service.VoteService;
 import com.sparta.bochodrive.global.entity.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,13 +17,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class VoteController {
 
-    private final VoteServiceImpl voteService;
+    private final VoteService voteService;
+
 
 
     //투표 참여
     @PostMapping
     public ApiResponse participateVote(@Valid @RequestBody VoteRequestDto voteRequestDto,
-                                  @AuthenticationPrincipal CustomUserDetails userDetails) {
+                                       @AuthenticationPrincipal CustomUserDetails userDetails) {
         voteService.participateVote(voteRequestDto,userDetails.getUser());
         return ApiResponse.ok(HttpStatus.OK.value(),"투표에 참여하셨습니다.");
     }
@@ -33,7 +34,8 @@ public class VoteController {
 
     //투표 취소
     @DeleteMapping("{id}")
-    public ApiResponse cancelVote(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ApiResponse cancelVote(@PathVariable("id") Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+
         voteService.cancelVote(id,userDetails.getUser());
         return ApiResponse.ok(HttpStatus.OK.value(),"투표를 취소하셨습니다.");
 
--- src/main/java/com/sparta/bochodrive/domain/vote/service/VoteServiceImpl.java
+++ src/main/java/com/sparta/bochodrive/domain/vote/service/VoteServiceImpl.java
@@ -1,36 +1,60 @@
+
 package com.sparta.bochodrive.domain.vote.service;
 
+import com.sparta.bochodrive.domain.community.entity.Community;
+import com.sparta.bochodrive.domain.community.repository.CommunityRepository;
+import com.sparta.bochodrive.domain.community.service.CommunityService;
+import com.sparta.bochodrive.domain.community.service.CommunityServiceImpl;
 import com.sparta.bochodrive.domain.user.entity.User;
 import com.sparta.bochodrive.domain.vote.dto.VoteRequestDto;
 import com.sparta.bochodrive.domain.vote.entity.Vote;
 import com.sparta.bochodrive.domain.vote.repository.VoteRepository;
+import com.sparta.bochodrive.global.exception.DuplicateVoteException;
 import com.sparta.bochodrive.global.exception.ErrorCode;
 import com.sparta.bochodrive.global.exception.NotFoundException;
 import com.sparta.bochodrive.global.exception.UnauthorizedException;
 import com.sparta.bochodrive.global.function.CommonFuntion;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class VoteServiceImpl implements VoteService {
 
-    private VoteRepository voteRepository;
-    private CommonFuntion commonFuntion;
+    private final VoteRepository voteRepository;
+    private final CommonFuntion commonFuntion;
+    private final CommunityServiceImpl communityService;
 
 
     @Override
     public void participateVote(VoteRequestDto voteRequestDto, User user) {
 
         commonFuntion.existsById(user.getId());
-        Vote vote=new Vote(voteRequestDto);
-        voteRepository.save(vote);
 
+        // 중복 투표 예외처리 로직
+        Optional<Vote> existingVote = voteRepository.findByUserIdAndCommunityId(voteRequestDto.getUserId(), voteRequestDto.getCommunityId());
+        if (existingVote.isPresent()) {
+            throw new DuplicateVoteException(ErrorCode.VOTE_NOT_DUPLICATE);
+        }
+
+        Community community = communityService.findCommunityById(voteRequestDto.getCommunityId());
+
+        Vote vote = new Vote(voteRequestDto, community, user);
+
+        voteRepository.save(vote);
 
     }
 
+
+
+    //투표 취소
     @Override
     public void cancelVote(Long id, User user) {
         commonFuntion.existsById(user.getId());
