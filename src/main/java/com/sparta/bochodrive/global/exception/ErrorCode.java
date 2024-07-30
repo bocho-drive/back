ec88f9976d1587473c3bbb3f1b5dbdd7645f275e
young219257 <young219257@gmail.com>
2024-07-30 14:59:14 +0900

    [Feat] 중복 투표 예외 에러 코드 추가

--- src/main/java/com/sparta/bochodrive/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/bochodrive/global/exception/ErrorCode.java
@@ -15,7 +15,8 @@ public enum ErrorCode {
     UPDATE_FAILED(403,"수정에 실패하였습니다."),
     DELETE_FAILED(403,"삭제에 실패하였습니다."),
     VOTE_FAILED(403,"투표 실패"),
-    VOTE_NOT_FOUND(404, "존재하지 않은 투표입니다.");
+    VOTE_NOT_FOUND(404, "존재하지 않은 투표입니다."),
+    VOTE_NOT_DUPLICATE(400,"중복 투표는 불가능합니다.");
 
     private final int status;
     private final String message;
