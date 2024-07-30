302d904204761722c0dfad386ce8537f0e0ead1c
young219257 <young219257@gmail.com>
2024-07-30 13:52:35 +0900

    [Refactor] agreeYN->agreeYn으로 컬럼명 변경

--- src/main/java/com/sparta/bochodrive/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/bochodrive/global/exception/ErrorCode.java
@@ -14,7 +14,9 @@ public enum ErrorCode {
     GETLIST_FAILED(403, "조회 실패하였습니다."),
     UPDATE_FAILED(403,"수정에 실패하였습니다."),
     DELETE_FAILED(403,"삭제에 실패하였습니다."),
-    VOTE_FAILED(403,"투표 실패");
+    VOTE_FAILED(403,"투표 실패"),
+    VOTE_NOT_FOUND(404, "존재하지 않은 투표입니다."),
+    VOTE_NOT_DUPLICATE(400,"중복 투표는 불가능합니다.");
 
     private final int status;
     private final String message;
