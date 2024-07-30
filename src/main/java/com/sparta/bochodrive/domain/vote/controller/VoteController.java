54bef38397baea6732106a85b8ff703ac4ea668c
young219257 <young219257@gmail.com>
2024-07-30 13:53:40 +0900

    [Refactor] 메소드 매개변수 타입 CustomUserDetails로 변경

--- src/main/java/com/sparta/bochodrive/domain/vote/controller/VoteController.java
+++ src/main/java/com/sparta/bochodrive/domain/vote/controller/VoteController.java
@@ -1,10 +1,9 @@
 package com.sparta.bochodrive.domain.vote.controller;
 
+import com.sparta.bochodrive.domain.security.model.CustomUserDetails;
 import com.sparta.bochodrive.domain.vote.dto.VoteRequestDto;
 
 import com.sparta.bochodrive.domain.vote.service.VoteService;
-import com.sparta.bochodrive.domain.vote.service.VoteServiceImpl;
-import com.sparta.bochodrive.global.UserDetailsImpl;
 import com.sparta.bochodrive.global.entity.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +17,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class VoteController {
 
-    private final VoteServiceImpl voteService;
+    private final VoteService voteService;
+
 
 
     //투표 참여
     @PostMapping
     public ApiResponse participateVote(@Valid @RequestBody VoteRequestDto voteRequestDto,
-                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
+                                  @AuthenticationPrincipal CustomUserDetails userDetails) {
         voteService.participateVote(voteRequestDto,userDetails.getUser());
         return ApiResponse.ok(HttpStatus.OK.value(),"투표에 참여하셨습니다.");
     }
@@ -34,7 +34,8 @@ public class VoteController {
 
     //투표 취소
     @DeleteMapping("{id}")
-    public ApiResponse cancelVote(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ApiResponse cancelVote(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+
         voteService.cancelVote(id,userDetails.getUser());
         return ApiResponse.ok(HttpStatus.OK.value(),"투표를 취소하셨습니다.");
 
