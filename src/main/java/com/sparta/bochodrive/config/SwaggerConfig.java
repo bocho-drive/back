d61653823b4c935b1c7b8bfddbf50c06d5462ee2
SUIN <65757661+whtndls@users.noreply.github.com>
2024-07-31 11:56:34 +0900

    [Feat] SwaggerConfig에 Authorize 토큰 기능 추가

--- src/main/java/com/sparta/bochodrive/config/SwaggerConfig.java
+++ src/main/java/com/sparta/bochodrive/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.sparta.bochodrive.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,15 +13,26 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(customOpenAPI());
+                .components(new Components().addSecuritySchemes("BearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .info(customOpenAPI())
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
     }
+
 
     @Bean
     public Info customOpenAPI() {
         return new Info()
-                .title("Spring Boot REST API Specifications")
-                .description("Specification")
+                .title("Bocho-Community-Service-REST API")
+                .description("보초 커뮤니티 API Swagger")
                 .version("1.0.0");
     }
+
 }
+
+
+
