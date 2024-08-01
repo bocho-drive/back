60a2cca46bcb7d316c549e07d1c052749bebe882
young219257 <young219257@gmail.com>
2024-08-01 16:29:09 +0900

    [Feat] s3 관련 이미지 추가, 삭제 테스트 컨트롤러 추가

--- src/main/java/com/sparta/bochodrive/domain/imageS3/controller/ImageS3Controller.java
+++ src/main/java/com/sparta/bochodrive/domain/imageS3/controller/ImageS3Controller.java
@@ -19,20 +19,23 @@ import java.util.stream.Collectors;
 public class ImageS3Controller {
     private final ImageS3Service imageS3Service;
 
+    //S3 테스트를 위한 컨트롤러입니다!!!!!
+
+
     //이미지 삽입
     @PostMapping
     public ApiResponse<String> uploadFiles(@RequestParam("files") MultipartFile files) throws IOException {
 
-        String url=imageS3Service.upload(files,"static");
+        String url=imageS3Service.upload(files);
        return ApiResponse.ok(HttpStatus.OK.value(), "업로드 성공하셨습니다.", url);
     }
-//
-//
-//    //이미지 삭제
-//    @DeleteMapping("{id}")
-//    public ApiResponse deleteFile(@PathVariable("id") Long id) throws IOException {
-//        imageS3Service.deleteFile(id);
-//        return ApiResponse.ok(HttpStatus.OK.value(), "이미지 삭제 성공하셨습니다.");
-//    }
+
+
+    //이미지 삭제
+    @DeleteMapping("{id}")
+    public ApiResponse deleteFile(@PathVariable("id") Long id) throws IOException {
+        imageS3Service.deleteFile(id);
+        return ApiResponse.ok(HttpStatus.OK.value(), "이미지 삭제 성공하셨습니다.");
+    }
 
 }
