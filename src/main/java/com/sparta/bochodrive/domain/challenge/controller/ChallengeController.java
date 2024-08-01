cf41baeb232a11ac33d83edc7c7f9adff24f6f78
young219257 <young219257@gmail.com>
2024-08-01 21:59:52 +0900

    [Remove] challenge 상세 조회, 목록 조회 삭제

--- src/main/java/com/sparta/bochodrive/domain/challenge/controller/ChallengeController.java
+++ src/main/java/com/sparta/bochodrive/domain/challenge/controller/ChallengeController.java
@@ -1,7 +1,6 @@
 package com.sparta.bochodrive.domain.challenge.controller;
 
-import com.sparta.bochodrive.domain.challenge.dto.ChallengeListResponseDto;
-import com.sparta.bochodrive.domain.challenge.entity.Challenge;
+import com.sparta.bochodrive.domain.challenge.dto.ChallengeResponseDto;
 import com.sparta.bochodrive.domain.challenge.service.ChallengeServiceImpl;
 import com.sparta.bochodrive.global.entity.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,28 +8,26 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/auth/challenges")
+@RequestMapping("/challenges")
 @RequiredArgsConstructor
 public class ChallengeController {
 
     private final ChallengeServiceImpl challengeService;
 
     @GetMapping
-    public ApiResponse<Page<ChallengeListResponseDto>> getChallengeList(@RequestParam(value = "page", defaultValue = "0") int page,
+    public ApiResponse<Page<ChallengeResponseDto>> getChallengeList(@RequestParam(value = "page", defaultValue = "0") int page,
                                                                         @RequestParam(value = "size", defaultValue = "10") int size,
                                                                         @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy,
                                                                         @RequestParam(value = "isAsc", defaultValue = "false") boolean isAsc) {
-        challengeService.getChallengeList(page,size,sortBy,isAsc);
-        return ApiResponse.ok(HttpStatus.OK.value(),"챌린지 목록 조회에 성공하셨습니다.");
+        Page<ChallengeResponseDto> challengeResponseDtos=challengeService.getChallengeList(page,size,sortBy,isAsc);
+        return ApiResponse.ok(HttpStatus.OK.value(),"챌린지 목록 조회에 성공하셨습니다.",challengeResponseDtos);
     }
 
     @GetMapping("{id}")
-    public ApiResponse getChallenge(@PathVariable("id") Long id) {
-        challengeService.getChallenge(id);
-        return ApiResponse.ok(HttpStatus.OK.value(), "챌린지 상세 조회에 성공하셨습니다.");
+    public ApiResponse<ChallengeResponseDto> getChallenge(@PathVariable("id") Long id) {
+        ChallengeResponseDto challengeResponseDto=challengeService.getChallenge(id);
+        return ApiResponse.ok(HttpStatus.OK.value(), "챌린지 상세 조회에 성공하셨습니다.",challengeResponseDto);
     }
 
 
