1fa7c509103b62d0f0917b7b7348e017ef62eb13
young219257 <young219257@gmail.com>
2024-08-02 18:18:58 +0900

    [Refactor] @Getter 추가

--- src/main/java/com/sparta/bochodrive/domain/vote/dto/VoteResponseDto.java
+++ src/main/java/com/sparta/bochodrive/domain/vote/dto/VoteResponseDto.java
@@ -1,7 +1,13 @@
 package com.sparta.bochodrive.domain.vote.dto;
 
 import com.sparta.bochodrive.domain.vote.entity.Vote;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VoteResponseDto {
     private Long userId;
     private boolean agreeYn;
