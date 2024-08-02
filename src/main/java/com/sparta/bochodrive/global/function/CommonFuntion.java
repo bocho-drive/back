b43bc59b44c91b93bccf680c67e2c580c09323eb
young219257 <young219257@gmail.com>
2024-08-02 18:17:04 +0900

    [Refactor] 게시글의 deleteyn=true인지 검사하는 메소드 추가

--- src/main/java/com/sparta/bochodrive/global/function/CommonFuntion.java
+++ src/main/java/com/sparta/bochodrive/global/function/CommonFuntion.java
@@ -1,8 +1,11 @@
 package com.sparta.bochodrive.global.function;
 
+import com.sparta.bochodrive.domain.community.entity.Community;
+import com.sparta.bochodrive.domain.community.repository.CommunityRepository;
 import com.sparta.bochodrive.domain.user.repository.UserRepository;
 import com.sparta.bochodrive.global.exception.ErrorCode;
 import com.sparta.bochodrive.global.exception.NotFoundException;
+import com.sparta.bochodrive.global.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +13,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CommonFuntion {
     private final UserRepository userRepository;
+    private final CommunityRepository communityRepository;
 
     //인가된 user가 userRepository에 없을 때 사용하는 메소드
     public void existsById(Long id) {
         userRepository.findById(id).orElseThrow(()->new NotFoundException(ErrorCode.USER_NOT_FOUND));
+    }
+    public void deleteCommunity(Long id) {
+        Community community = communityRepository.findById(id).orElseThrow(()->new NotFoundException(ErrorCode.POST_NOT_FOUND));
+        if(community.isDeleteYN()==true){
+            throw new ResourceNotFoundException(ErrorCode.COMMUNITY_DELETE);
+
+        }
     }
 
 }
