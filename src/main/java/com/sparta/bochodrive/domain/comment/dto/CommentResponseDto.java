169647d4171d2ee2115c9bfc1f2b1dc9db62ae68
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-03 09:38:02 +0900

    [Refactor] @Builder 추가

--- src/main/java/com/sparta/bochodrive/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/bochodrive/domain/comment/dto/CommentResponseDto.java
@@ -3,6 +3,7 @@ package com.sparta.bochodrive.domain.comment.dto;
 
 import com.sparta.bochodrive.domain.comment.entity.Comment;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CommentResponseDto {
     private Long id;
     private String content;
