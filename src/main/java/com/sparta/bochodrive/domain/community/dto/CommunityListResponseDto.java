3eaeb09da2decb61f52da4e7da84dc6b63e45d36
young219257 <young219257@gmail.com>
2024-08-03 19:24:05 +0900

    [Refactor] challengeVarify 수정

--- src/main/java/com/sparta/bochodrive/domain/community/dto/CommunityListResponseDto.java
+++ src/main/java/com/sparta/bochodrive/domain/community/dto/CommunityListResponseDto.java
@@ -1,5 +1,6 @@
 package com.sparta.bochodrive.domain.community.dto;
 
+import com.sparta.bochodrive.domain.challengevarify.entity.ChallengeVarify;
 import com.sparta.bochodrive.domain.community.entity.Community;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,4 +33,13 @@ public class CommunityListResponseDto {
         this.likeCount=community.getLikeCount();
     }
 
+    public CommunityListResponseDto(ChallengeVarify challengeVarify) {
+        this.id=challengeVarify.getId();
+        this.title=challengeVarify.getCommunity().getTitle();
+        this.author=challengeVarify.getCommunity().getUser().getNickname();
+        this.createdAt=challengeVarify.getCreatedAt();
+        this.verifiedYN=challengeVarify.getCommunity().isVerifiedYN();
+        this.viewCount=challengeVarify.getCommunity().getViewCount();
+        this.likeCount=challengeVarify.getCommunity().getLikeCount();
+    }
 }
