7649e2520a57a19cdfe9f472cddd2197624ce8e9
young219257 <young219257@gmail.com>
2024-08-06 17:29:46 +0900

    [Feat] MyPageChallengeVarifyListResponseDto 생성

---
+++ src/main/java/com/sparta/bochodrive/domain/mypage/dto/MyPageChallengeVarifyListResponseDto.java
@@ -0,0 +1,16 @@
+package com.sparta.bochodrive.domain.mypage.dto;
+
+import com.sparta.bochodrive.domain.challenge.entity.Challenge;
+import com.sparta.bochodrive.domain.challengevarify.entity.ChallengeVarify;
+import com.sparta.bochodrive.domain.community.dto.CommunityListResponseDto;
+
+public class MyPageChallengeVarifyListResponseDto extends CommunityListResponseDto {
+
+    private Long challengeId;
+
+    public MyPageChallengeVarifyListResponseDto(ChallengeVarify challengeVarify) {
+        super(challengeVarify);
+        this.challengeId = challengeVarify.getId();
+    }
+}
+
