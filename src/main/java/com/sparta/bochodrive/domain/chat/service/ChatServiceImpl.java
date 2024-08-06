7cb44164cbf8a0d44b38363cf3f735a07d565281
DESKTOP-HSB9VFF\user <garyseo1@naver.com>
2024-08-07 05:03:15 +0900

    chat 이거 쓸지 안쓸지 미완

--- src/main/java/com/sparta/bochodrive/domain/chat/service/ChatServiceImpl.java
+++ src/main/java/com/sparta/bochodrive/domain/chat/service/ChatServiceImpl.java
@@ -6,11 +6,11 @@ import com.sparta.bochodrive.domain.drivematchingapply.entity.DriveMatchingApply
 import com.sparta.bochodrive.domain.drivematchingapply.service.DriveMatchingApplyService;
 import com.sparta.bochodrive.domain.security.model.CustomUserDetails;
 import com.sparta.bochodrive.domain.user.entity.User;
-import com.sparta.bochodrive.domain.user.repository.UserRepository;
 import com.sparta.bochodrive.global.function.CommonFuntion;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,15 +21,28 @@ public class ChatServiceImpl implements ChatService {
     private final ChatRepository chatRepository;
     //private final UserRepository userRepository;
 
-
+    /**
+     * 특정 방에 채팅 메시지를 전송합니다.
+     *
+     * @param roomId      채팅 방 ID
+     * @param message     전송할 메시지
+     * @param userDetails 메시지를 전송하는 사용자의 세부 정보
+     */
     @Override
+    @Transactional
     public void sendMessage(Long roomId, String message, CustomUserDetails userDetails) {
-        //테스트용
-        //User user = userRepository.findById(2L).get();
         User user = userDetails.getUser();
+
+        // 방에 대한 사용자의 권한을 확인합니다.
         driveMatchingApplyService.validPermission(roomId, user);
+
+        // 사용자가 존재하는지 확인합니다.
         commonFuntion.existsById(user.getId());
+
+        // 드라이브 매칭 신청 엔티티를 조회합니다.
         DriveMatchingApply driveMatchingApply = driveMatchingApplyService.getDriveMatching(roomId);
+
+        // 채팅 메시지를 생성하고 저장합니다.
         Chat chat = Chat.builder()
                 .message(message)
                 .user(user)
@@ -37,9 +50,18 @@ public class ChatServiceImpl implements ChatService {
                 .build();
         chatRepository.save(chat);
 
+        log.info("방 {}에 메시지를 전송했습니다: {}", roomId, message);
     }
 
+    /**
+     * 채팅 방 ID로 채팅 방을 찾습니다.
+     *
+     * @param roomId 채팅 방 ID
+     * @return 주어진 방 ID에 해당하는 DriveMatchingApply 엔티티
+     * @throws IllegalArgumentException 방을 찾을 수 없는 경우
+     */
     @Override
+    @Transactional(readOnly = true)
     public DriveMatchingApply findRoomById(Long roomId) {
         return driveMatchingApplyService.getDriveMatching(roomId);
     }
