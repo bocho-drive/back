9067a4e03482fe4658466eecdfe09c9d5a1251e8
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-06 11:05:59 +0900

    [Feat] OAuth2 컨트롤러

--- src/main/java/com/sparta/bochodrive/domain/OAuth/OAuth2Controller.java
+++ src/main/java/com/sparta/bochodrive/domain/OAuth/OAuth2Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class TestController {
+public class OAuth2Controller {
 
     @GetMapping("/api/oauth/login/kakao")
     @ResponseBody
