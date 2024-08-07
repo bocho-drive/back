0e9d7eb686f762a2a68977f63f3742a29af8c6d6
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-07 21:17:25 +0900

    [Feat] 마이페이지 API 수정사항 적용 - 프로필 별도 조회 컨트롤러 - 카테고리별 게시글 조회 기능

--- src/main/java/com/sparta/bochodrive/domain/mypage/dto/MypageCommunityListResponseDto.java
+++ src/main/java/com/sparta/bochodrive/domain/mypage/dto/MypageCommunityListResponseDto.java
@@ -2,32 +2,21 @@ package com.sparta.bochodrive.domain.mypage.dto;
 
 
 import com.sparta.bochodrive.domain.community.dto.CommunityListResponseDto;
+import com.sparta.bochodrive.domain.community.entity.CategoryEnum;
 import com.sparta.bochodrive.domain.community.entity.Community;
-import com.sparta.bochodrive.domain.user.entity.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
-import java.time.LocalDateTime;
-
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class MypageCommunityListResponseDto {
 
-    private Long id;
-    private String nickname;
-    private String email;
-    private LocalDateTime createdAt;
+    private CategoryEnum category;
     private Page<CommunityListResponseDto> communityListResponseDto;
 
-    public MypageCommunityListResponseDto (User user, Page<Community> communities) {
-        this.id = user.getId();
-        this.nickname = user.getNickname();
-        this.email = user.getEmail();
-        this.createdAt = user.getCreatedAt();
-        this.communityListResponseDto = communities.map(CommunityListResponseDto::new) ;
+    public MypageCommunityListResponseDto(CategoryEnum category, Page<Community> communities) {
+        this.category = category;
+        this.communityListResponseDto = communities.map(CommunityListResponseDto::new);
     }
-
 }
--- src/main/java/com/sparta/bochodrive/domain/mypage/service/MyPageService.java
+++ src/main/java/com/sparta/bochodrive/domain/mypage/service/MyPageService.java
@@ -1,14 +1,17 @@
 package com.sparta.bochodrive.domain.mypage.service;
 
 import com.sparta.bochodrive.domain.comment.dto.CommentResponseDto;
-import com.sparta.bochodrive.domain.community.dto.CommunityListResponseDto;
+import com.sparta.bochodrive.domain.community.entity.CategoryEnum;
 import com.sparta.bochodrive.domain.mypage.dto.MyPageChallengeVarifyListResponseDto;
+import com.sparta.bochodrive.domain.mypage.dto.MyProfileResponseDto;
 import com.sparta.bochodrive.domain.mypage.dto.MypageCommunityListResponseDto;
 import org.springframework.data.domain.Page;
 
 public interface MyPageService {
+    MyProfileResponseDto getMyProfile(Long id);
+
     // 게시글 목록 불러오기
-    MypageCommunityListResponseDto getMyPosts(Long userid, int page, int size, String sortBy, boolean isAsc);
+    MypageCommunityListResponseDto getMyPosts(Long userid, int page, int size, String sortBy, boolean isAsc, CategoryEnum category);
 
     // 댓글 목록 불러오기
     Page<CommentResponseDto> getMyComments(Long userId, int page, int size, String sortBy, boolean isAsc);
