ebc3a03fa047ce9902b55e00167a351ac122cf99
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-08 10:58:26 +0900

    develop 브랜치 배포

--- src/main/java/com/sparta/bochodrive/BochodriveApplication.java
+++ src/main/java/com/sparta/bochodrive/BochodriveApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class BochodriveApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BochodriveApplication.class, args);
     }
