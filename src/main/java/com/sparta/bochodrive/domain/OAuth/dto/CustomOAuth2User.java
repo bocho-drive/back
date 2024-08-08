903e653e178f5bd098aa313d671b18131c84f849
young219257 <young219257@gmail.com>
2024-08-08 15:00:40 +0900

    [Refactor] ArrayList 삭제

--- src/main/java/com/sparta/bochodrive/domain/OAuth/dto/CustomOAuth2User.java
+++ src/main/java/com/sparta/bochodrive/domain/OAuth/dto/CustomOAuth2User.java
@@ -5,7 +5,6 @@ import com.sparta.bochodrive.domain.user.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
