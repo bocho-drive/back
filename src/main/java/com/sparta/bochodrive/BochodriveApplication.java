a6811a56a4baae4a6202c32ccb3e50714eac752b
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-12 21:38:37 +0900

    [Refactor] 타임존 변경

--- src/main/java/com/sparta/bochodrive/BochodriveApplication.java
+++ src/main/java/com/sparta/bochodrive/BochodriveApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class BochodriveApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
         SpringApplication.run(BochodriveApplication.class, args);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("현재시간 " + now);
     }
 }
