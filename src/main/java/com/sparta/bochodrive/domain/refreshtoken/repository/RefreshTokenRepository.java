34d2b0eb6a8c4287eb1ead146df253a1d85ee74a
young219257 <young219257@gmail.com>
2024-08-13 20:28:04 +0900

    [Feat] refreshToken 엔티티 추가

---
+++ src/main/java/com/sparta/bochodrive/domain/refreshtoken/repository/RefreshTokenRepository.java
@@ -0,0 +1,21 @@
+package com.sparta.bochodrive.domain.refreshtoken.repository;
+
+import com.sparta.bochodrive.domain.refreshtoken.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+
+    // 만료되지 않은 리프레시 토큰을 조회하는 메서드
+    @Query("SELECT rt FROM RefreshToken rt WHERE rt.refreshToken = :refreshToken AND rt.expiredAt > :now")
+    Optional<RefreshToken> findValidRefreshToken(@Param("refreshToken") String refreshToken, @Param("now") LocalDateTime now);
+
+    RefreshToken findByUserId(Long id);
+}
