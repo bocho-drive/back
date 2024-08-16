ed7d5fb27f49e91fcd614e8a28978518716e03e2
SUIN <65757661+whtndls@users.noreply.github.com>
2024-08-16 12:02:44 +0900

    [Refactor] Swagger 업데이트

--- src/main/java/com/sparta/bochodrive/global/config/SwaggerConfig.java
+++ src/main/java/com/sparta/bochodrive/global/config/SwaggerConfig.java
@@ -6,16 +6,21 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server.url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
 
         Server server = new Server();
-        server.setUrl("https://api.tteokip.o-r.kr");
+        server.setUrl(serverUrl);
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("BearerAuth",
                         new SecurityScheme()
